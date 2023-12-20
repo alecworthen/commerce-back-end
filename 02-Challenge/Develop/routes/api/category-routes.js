235f5e59c17bb2ef1c9bbeb65fca9bdf457e1a71
Alec Worthen <alecworthen@gmail.com>
2023-12-19 21:59:10 -0800

    updated routes

--- 02-Challenge/Develop/routes/api/category-routes.js
+++ 02-Challenge/Develop/routes/api/category-routes.js
@@ -3,26 +3,54 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    const categories = await Category.findAll({ include : [{ model : Product }] });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({message: 'error! not found!'});
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, { include: [{ model: Product }] });
+
+    if (!category) {
+      res.status(404).json({message: 'error! id not found!'});
+      return;
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json({message: 'error! not found!'});
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json({message: 'category creatoin failed'});
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updated = await Category.update(req.body, {where: {id: req.params.id}});
+    !updated[0] ? res.status(404).json({message: 'no id found!'}) : res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({message: 'failed to update!'});
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleted = await Category.destroy({where: {id: req.params.id}});
+    !deleted ? res.status(404).json({message: 'no id found!'}) : res.status(200).json(deleted);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- 02-Challenge/Develop/routes/api/tag-routes.js
+++ 02-Challenge/Develop/routes/api/tag-routes.js
@@ -1,28 +1,65 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product} = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{model: Product}],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({message: 'no tags found!'});
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    if (!tagData) {
+      res.status(404).json({message: 'id has no tag!'});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json({message: 'no tag found!'});
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json({message: 'Failure! No new Tag!'});
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updated = await Tag.update(req.body, {
+      where: {id: req.params.id},
+    });
+    !updated[0]
+      ? res.status(400).json({message: 'nno tag with this id!'})
+      : res.status(200).json(updated);
+  } catch (err) {
+    res.status(500).json({message: 'Failure to update tag!'})
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleted = await Tag.destroy({where: {id: req.params.id}});
+    !deleted
+      ? res.status(400).json({message: 'no tag with this id!'})
+      : res.status(200).json(deleted);
+  } catch (err) {
+    res.status(500).json({message: 'Failure to delete tag!'});
+  }
 });
 
 module.exports = router;
